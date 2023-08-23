43f1dcf3a2da258988952b3e1ffda5ec42b3156d
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-23 10:08:12 -0400

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Francesca MACDONALD on 2023-08-23.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
